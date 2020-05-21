2d9dfc376e887cd37f85cca3925255747cafdf62
scutJzh <745878318@qq.com>
2020-05-22 00:45:23 +0800

    搭建eureka服务集群

---
+++ mySpringCloud/eureka-server-8001/src/main/java/com/jzh/study/springcloud/eureka_server/EurekaServer8001.java
@@ -0,0 +1,14 @@
+package com.jzh.study.springcloud.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer8001.class, args);
+    }
+
+}
